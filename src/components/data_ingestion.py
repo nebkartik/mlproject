10bf6e966bf80b5e97a41c7ba83773dbd62b27f3
nebkartik <nebkartik@gmail.com>
2023-06-30 14:50:28 +0530

    Data Transformation Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
+#data class so that we dont need init to initiate variables inside a class
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifacts',"train.csv")
@@ -38,4 +40,6 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    datatransforamtion_obj = DataTransformation()
+    datatransforamtion_obj.initiate_data_transformation(train_data,test_data)
